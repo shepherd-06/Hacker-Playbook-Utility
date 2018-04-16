460e256be75dd55af77f98bc9011962da702fb6c
Ibtehaz <ibtehaz.shawon@gmail.com>
2018-04-17 03:16:03 +0600

    asked sudo permission, update, upgrade bash file created.

---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import subprocess
+import stat
+
+
+def ask_sudo():
+    """
+
+    :return: if the current user has root privileges or not!
+    """
+    is_sudo = False
+    try:
+        if not os.path.exists('/etc/foo'):
+            os.makedirs('/etc/foo')
+        else:
+            os.rename('/etc/foo', '/etc/bar')
+        is_sudo = True
+    except IOError as e:
+        if 'Permission denied' in str(e):
+            print("You need root permissions to do this (O_o)")
+    finally:
+        if os.path.exists('/etc/foo'):
+            os.rmdir('/etc/foo')
+        if os.path.exists('/etc/bar'):
+            os.rmdir('/etc/bar')
+        return is_sudo
+
+
+def system_update_upgrade():
+    """
+
+    :return:
+    """
+    try:
+        file_status = os.stat('updater.sh')
+        os.chmod('updater.sh', file_status.st_mode| stat.S_IEXEC)
+        subprocess.call(['./updater.sh'])
+    except IOError as error:
+        sys.exit(str(error))
+    except subprocess.CalledProcessError as error:
+        sys.exit(str(error))
+    except OSError as error:
+        sys.exit((str(error)))
+    finally:
+        return 1
+
+
+if __name__ == '__main__':
+    if sys.version_info < (3, 0, 0):
+        sys.stderr.write("You need python 3.0.0 or later to run this script\n")
+        exit(1)
+    if ask_sudo():
+        system_update_upgrade()
+        print("Hello!")
+    else:
+        sys.exit("Permission Denied!")
---
+++ updater.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#TODO: need some design here
+echo "Running the update command"
+sudo apt update
+#TODO: need some design here
+echo "update complete"
+#TODO: need some design here
+echo "Running Upgrade"
+#TODO: need some design here | user confirmation is needed here.
+sudo apt upgrade
+#TODO: need some design here
+echo "Upgrade Complete"
