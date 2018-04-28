c40ed149e018b25bd7af0e50190da97028813d18
Ibtehaz <ibtehaz.shawon@gmail.com>
2018-04-28 22:11:32 +0600

    checking one test case in tracisCI

--- test_utility.py
+++ test_utility.py
@@ -5,13 +5,15 @@ from HP_Utility import Utility
 class TestUtility(TestCase):
 
     def test_is_root(self):
-        self.assertFalse(Utility().is_root(), "Script is not root enabled for the machine!")
+        pass
+        # self.assertFalse(Utility().is_root(), "Script is not root enabled for the machine!")
 
     def test_system_upgrade(self):
         self.assertEqual(Utility().system_upgrade(True), 0)
 
     def test_install_psql(self):
-        self.assertEqual(Utility.install_psql(True), 0)
+        pass
+        # self.assertEqual(Utility.install_psql(True), 0)
 
     def test_install_metasploit(self):
         # self.assertTrue(Utility.install_metasploit(True), 0)
